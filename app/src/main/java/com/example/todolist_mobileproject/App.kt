e2dd6c345979ff79f3dd0933d5f76cb6f000d5bf
Leoj Anidem <jeremykaizer@gmail.com>
2024-06-15 15:27:49 -0400

    [Frontend] Cambio de estilo en los componentes principales

--- app/src/main/java/com/example/todolist_mobileproject/App.kt
+++ app/src/main/java/com/example/todolist_mobileproject/App.kt
@@ -1,12 +1,10 @@
 package com.example.todolist_mobileproject
 
-import PopupMenuHelper
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 
 // Renderizar elementos mutables
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 // Elementos de la app
@@ -18,6 +16,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class App : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +24,9 @@ class App : ComponentActivity() {
 
         // ---------------------------------------------
         // RecyclerView es un elemento xml que permite mostrar una lista de elementos
-        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
-        val gridLayoutManager = GridLayoutManager(this, 2)
-        recyclerView.layoutManager = gridLayoutManager
+        val recyclerView: RecyclerView = findViewById(R.id.list_recycler_view)
+        val linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
 
         // Crea una lista de elementos a renderizar // Reemplazar por API
         val items = mutableListOf(
@@ -43,16 +42,16 @@ class App : ComponentActivity() {
 
 
         // Menu de filtros
-        val menuButton: Button = findViewById(R.id.filter_button)
-        menuButton.setOnClickListener { view ->
-            // Actualiza el texto que se muestra en el menu
-            PopupMenuHelper.showPopupMenu(this, view) {selectedItemTitle ->
-                menuButton.text = selectedItemTitle
-            }
-
-            // Aqui se deberia modificar el arreglo de Todos, para dependiendo del filtro
-            // mostrar solo los elementos que correspondan
-        }
+        //        val menuButton: Button = findViewById(R.id.filter_button)
+        //        menuButton.setOnClickListener { view ->
+        //            // Actualiza el texto que se muestra en el menu
+        //            PopupMenuHelper.showPopupMenu(this, view) {selectedItemTitle ->
+        //                menuButton.text = selectedItemTitle
+        //            }
+        //
+        //            // Aqui se deberia modificar el arreglo de Todos, para dependiendo del filtro
+        //            // mostrar solo los elementos que correspondan
+        //        }
 
 
         // ---------------------------------------------
